58f71cebdc42d71838b161be9514404daf26eab2
Ivaniel-Dz <pcivaniel@gmail.com>
2024-11-06 23:29:23 -0500

    Update Views

---
+++ PasswordManager/wwwroot/js/modal.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+	var modal = document.getElementById("customModal");
+	var btn = document.getElementById("openModalButton");
+	var span = document.querySelector(".close-button");
+
+	btn.onclick = function () {
+		modal.style.display = "block";
+	}
+
+	span.onclick = function () {
+		modal.style.display = "none";
+	}
+
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+});
