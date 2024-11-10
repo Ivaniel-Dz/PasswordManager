271fc7e3c80d70c233e93d5b604a9ed5b1da5261
Ivaniel-Dz <pcivaniel@gmail.com>
2024-11-10 01:55:51 -0500

    update view style

---
+++ PasswordManager/wwwroot/js/detalle.js
@@ -0,0 +1,12 @@
+﻿function togglePassword() {
+    const hiddenPassword = document.getElementById("password-hidden");
+    const visiblePassword = document.getElementById("password-visible");
+
+    if (hiddenPassword.style.display === "none") {
+        hiddenPassword.style.display = "inline";
+        visiblePassword.style.display = "none";
+    } else {
+        hiddenPassword.style.display = "none";
+        visiblePassword.style.display = "inline";
+    }
+}
