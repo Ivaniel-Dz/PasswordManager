5b69049897bcc66675eef0475bde3946344b2ceb
Ivaniel-Dz <pcivaniel@gmail.com>
2025-04-18 14:30:32 -0500

    creacion de component

---
+++ PasswordClient/src/app/pages/password/password-detail/password-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-detail',
+  imports: [],
+  templateUrl: './password-detail.component.html',
+  styleUrl: './password-detail.component.css'
+})
+export class PasswordDetailComponent {
+
+}
---
+++ PasswordClient/src/app/pages/tarjeta/tarjeta-form/tarjeta-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-form',
+  imports: [],
+  templateUrl: './tarjeta-form.component.html',
+  styleUrl: './tarjeta-form.component.css'
+})
+export class TarjetaFormComponent {
+
+}
