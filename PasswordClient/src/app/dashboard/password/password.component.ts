7513f95d20279acbfeb03060de2e8449e81f9db7
Ivaniel-Dz <pcivaniel@gmail.com>
2025-04-18 03:56:43 -0500

    creación de la tabla component

--- PasswordClient/src/app/dashboard/password/password.component.ts
+++ PasswordClient/src/app/dashboard/password/password.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../layouts/header/header.component';
 import { CardComponent } from '../../components/card/card.component';
+import { FooterComponent } from '../../layouts/footer/footer.component';
 
 @Component({
   selector: 'app-password',
-  imports: [HeaderComponent, CardComponent],
+  imports: [HeaderComponent, CardComponent, FooterComponent,],
   templateUrl: './password.component.html',
   styleUrl: './password.component.css'
 })
--- PasswordClient/src/app/dashboard/tarjetas/tarjetas.component.ts
+++ PasswordClient/src/app/dashboard/tarjetas/tarjetas.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../layouts/header/header.component';
+import { TableComponent } from '../../components/table/table.component';
+import { FooterComponent } from '../../layouts/footer/footer.component';
 
 @Component({
   selector: 'app-tarjetas',
-  imports: [HeaderComponent,],
+  imports: [HeaderComponent, TableComponent, FooterComponent,],
   templateUrl: './tarjetas.component.html',
   styleUrl: './tarjetas.component.css'
 })
