338511c1ebc8bb537e88eec9ba1df08a5834dfc4
Ivaniel-Dz <pcivaniel@gmail.com>
2025-06-12 21:47:22 -0500

    update app.config

--- PasswordClient/src/app/app.config.ts
+++ PasswordClient/src/app/app.config.ts
@@ -1,15 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     // Proporciona el cliente HTTP para realizar peticiones a la API
-    provideHttpClient(withInterceptors([authInterceptor])),
-  ]
+    provideHttpClient(),
+  ],
 };
